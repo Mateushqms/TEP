8a2e618f9ae034beb55fa31509c8bdf9999d43ce
Mateushqms <mateushqms@gmail.com>
2025-09-09 16:00:07 -0300

    more

--- Lista_2/D.cpp
+++ Lista_2/D.cpp
@@ -2,41 +2,41 @@
 
 using namespace std;
 
-vector<int> found_zero(const vector<int>& v) {
-    vector<int> indice_0;
-    for (size_t i = 0; i < v.size(); i++) {
-        if (v[i] == 0) {
-            indice_0.emplace_back(i);
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> fita(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> fita[i];
+    }
+
+    vector<int> dist(N, 10000);
+
+    for (int i = 0; i < N; ++i) {
+        if (fita[i] == 0) {
+            dist[i] = 0;
+        } else if (i > 0) {
+            dist[i] = dist[i - 1] + 1;
         }
     }
-    return indice_0;
-}
-int main() {
-    ios::sync_with_stdio(false);
-    
-    int qnt_quadrados;
-    cin >> qnt_quadrados;
 
-    vector<int> v(qnt_quadrados);
-
-    for(int i = 0; i < qnt_quadrados; i++){
-        cin >> v[i];
+    for (int i = N - 2; i >= 0; --i) {
+        if (fita[i] == 0) {
+            dist[i] = 0;
+        } else {
+            dist[i] = min(dist[i], dist[i + 1] + 1);
+        }
     }
 
-    vector<int> indices_onde_tem_0 = found_zero(v);
-
-    //-1 0 -1 -1 -1 -1 -1 -1 -1 -1 0 -1 -1
-    // 1 - 10 = 4 
-    //0 -1 -1 -1 -1 -1 -1 -1 0
-    //0 8 /2 = 4  
-    /*o que pode ser feito??
-    eu posso verificar os da esquerda do primeiro NÃO DA ERRO
-    eu posso pintar os da direita com os respectivos números até encontrar um 0
-    se eu encontrar um zero agora eu verifico a esquerda do novo 0 se os da esquerda será preenchido com um número maior ou igual eu não pinto o quadrado
-    1 0 1 2 3 4 3 2 1 0
-    */
-    
-    
+    for (int i = 0; i < N; ++i) {
+        if (dist[i] >= 9) {
+            cout << 9 << " ";
+        } else {
+            cout << dist[i] << " ";
+        }
+    }
+    cout << endl;
 
     return 0;
 }
